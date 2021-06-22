4ec6cc58b12e68314fa6fea1569f7acc387b67ef
Yuliya Zhuravel <zhuravel.dev@gmail.com>
2021-06-22 16:26:00 +0300

    Add biometric

---
+++ app/src/main/java/com/example/studyprojectrnc/activities/BiometricActivity.kt
@@ -0,0 +1,58 @@
+package com.example.studyprojectrnc.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
+import com.example.studyprojectrnc.databinding.BiometricBinding
+import java.util.concurrent.Executor
+
+class BiometricActivity : AppCompatActivity() {
+
+    lateinit var binding: BiometricBinding
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = BiometricBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        executor = ContextCompat.getMainExecutor(this)
+        biometricPrompt = BiometricPrompt(
+            this@BiometricActivity, executor, object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    binding.tvBiometric.text = "Authentication Error: $errString"
+                    Toast.makeText(this@BiometricActivity, "Authentication Error: $errString", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    binding.tvBiometric.text = "Authentication Failed"
+                    Toast.makeText(this@BiometricActivity,
+                        "Auth failed", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    binding.tvBiometric.text = "Authentication succeed"
+                    Toast.makeText(
+                        this@BiometricActivity, "Auth succeed", Toast.LENGTH_SHORT).show()
+                    startActivity(
+                        Intent(this@BiometricActivity, MainActivity::class.java)
+                    )
+                    finish()
+                }
+            })
+        promptInfo = BiometricPrompt.PromptInfo.Builder().setTitle("Biometric auth")
+            .setSubtitle("Login using fingerprint auth").setNegativeButtonText("Use app password")
+            .build()
+
+        binding.btnBiometric.setOnClickListener {
+            biometricPrompt.authenticate(promptInfo)
+        }
+    }
+}
--- app/src/main/java/com/example/studyprojectrnc/activities/SplashActivity.kt
+++ app/src/main/java/com/example/studyprojectrnc/activities/SplashActivity.kt
@@ -3,12 +3,24 @@ package com.example.studyprojectrnc.activities
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        startActivity(Intent(this, MainActivity::class.java))
+        if (canUseBiometric()) {
+            startActivity(Intent(this, BiometricActivity::class.java))
+        } else {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
         finish()
+    }
+
+    private fun canUseBiometric(): Boolean {
+        return BiometricManager.from(this)
+            .canAuthenticate(BIOMETRIC_STRONG or DEVICE_CREDENTIAL) == BiometricManager.BIOMETRIC_SUCCESS
     }
 }
